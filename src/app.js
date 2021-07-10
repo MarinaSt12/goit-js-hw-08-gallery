3d82ebb4986030037d8d05de48b615a94d4dc0e3
Marina Standriichuk <standriichuk.m@gmail.com>
2021-07-10 19:30:25 +0300

    Add the solution for the task

--- src/app.js
+++ src/app.js
@@ -63,3 +63,65 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+// ==========================================================================================================
+
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  modalCloseBtn: document.querySelector('[data-action="close-lightbox"]'),
+  modal: document.querySelector('.js-lightbox'),
+  overlay: document.querySelector('.lightbox__overlay'),
+  modalImage: document.querySelector('.lightbox__image'),
+};
+
+const images = galleryItems.reduce(
+  (str, { preview, original, description }) =>
+    str +
+    `<li class="gallery__item">
+      <a class="gallery__link" href=${original}>
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+      </a>
+    </li>`,
+  '',
+);
+
+refs.gallery.insertAdjacentHTML('afterbegin', images);
+refs.gallery.addEventListener('click', event => {
+  event.preventDefault();
+  const target = event.target;
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  onModalOpen(target);
+});
+
+function onModalOpen(target) {
+  refs.modal.classList.add('is-open');
+  refs.overlay.addEventListener('click', onModalClose);
+  window.addEventListener('keydown', onModalPress);
+  refs.modalCloseBtn.addEventListener('click', onModalClose);
+
+  galleryItems.forEach(({ preview, original, description }) => {
+    if (target.src === preview) {
+      refs.modalImage.src = `${original}`;
+      refs.modalImage.alt = `${description}`;
+    }
+  });
+}
+
+function onModalClose() {
+  refs.modal.classList.remove('is-open');
+  refs.modalImage.src = '';
+  refs.modalImage.alt = '';
+  window.removeEventListener('keydown', onModalPress);
+  refs.modalCloseBtn.removeEventListener('click', onModalClose);
+  refs.overlay.removeEventListener('click', onModalClose);
+}
+
+function onModalPress(event) {
+  const target = event;
+  if (event.key === 'Escape') {
+    onModalClose();
+  }
+  onChangeSlide(target);
+}
